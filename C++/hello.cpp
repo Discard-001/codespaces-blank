c816944e210a964dcd0ca00df2e19b6946fe9b97
搁浅 <107676765+Discard-001@users.noreply.github.com>
2023-01-28 02:18:46 +0000

    first commit

---
+++ C++/hello.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a[] = {1,2,3,8,2,2,3,4,8,4,4,};
+    cout << "hello" << endl;
+    for(int i : a) cout << i << " ";
+    cout << endl;
+    return 0;
+}
